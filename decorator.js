248496c83df67c79261dde73ba8a2ae4d4bb69cf
Rodion <79146934+Rrodya@users.noreply.github.com>
2023-05-02 16:56:36 +0500

    Create decorator.js

---
+++ decorator.js
@@ -0,0 +1,64 @@
+// Client --> Decorator(addOptions(), [ Component ]);
+
+class Car {
+  contstructor() {
+    this.price = 10000;
+    this.model = "Car";
+  }
+  
+  getPrice() {
+    return this.price;
+  }
+  
+  getDescription() {
+    return this.model;
+  }
+}
+
+class TeslaCar extends Car {
+  constructor() {
+    super();
+    this.price = 25000;
+    this.model = "Tesla";
+  }
+}
+
+class Autopilot {
+  constructor(car) {
+    this.car = car;
+  }
+  
+  getPrice() {
+    return this.car.getPrice() + 3000;
+  }
+  
+  getDescription() {
+    return `${this.car.getDescription()} with autopilot`;
+  }
+}
+
+class Parktronic {
+  constructor(car) {
+    this.car = car;
+  }
+  
+  getPrice() {
+    return this.car.getPrice() + 3000;
+  }
+  
+  getDescription() {
+    return `${this.car.getDescription()} with parktronic`;
+  }
+}
+
+let tesla = new Tesla();
+tesla = new Autlopilot(tesla);
+tesla = new Parktronic(tesla);
+
+console.log(tesla.getPrice(), tesla.getDescription());
+
+let tesla = new Tesla();
+tesla = new Autlopilot(tesla);
+
+console.log(tesla.getPrice(), tesla.getDescription());
+
